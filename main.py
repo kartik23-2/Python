29f7f6c41cab10766988ac56d53d8aeffd516011
kartik23-2 <kartikbaliyan00004@gmail.com>
2025-10-19 01:01:20 +0530

    inheritence,file handling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #import 
 import math                     #importing math module or any other if it is in same directory
-from math import sqrt           #importing only specific function from module
+from math import sqrt       #importing only specific function from module
 # import sys                                                                                        #importing sys module to get the path of python interpreter
 # sys.path.append('C:\\Users\\Kartik\\AppData\\Local\\Programs\\Python\\Python310\\Lib')            #adding path of module if not in same directory
 
@@ -26,6 +26,20 @@ class Students():
         return cls(name, int(student_no))
 
 
+    @staticmethod  #static methods do not take instance or class as first argument
+    def is_adult(age):
+        return age > 18 
+    
+class Monitor(Students):   #inheritance(it inherits all properties of Students class and we can overwrite them if needed)
+    def __init__(self, name, student_no):
+        super().__init__(name, student_no)  #calling parent class constructor to initialize name and student_no
+        self.student_no = 0  # This creates an INSTANCE variable with value 0
+
+
+
+
+
+
 print(Students.no_of_student)  
 
 stu_1 = Students("Kartik", 2410167)
@@ -33,7 +47,7 @@ stu_2 = Students("Rahul", 2410168)
 
 stu_str_1 = "Ankit-2410169"   #using alternative constructor
 stu_3 = Students.from_str(stu_str_1)
-
+stu_4 = Monitor("Ankit", 2410169)
 print(Students.no_of_student)  #to check number of students after creating two objects
 
 print(stu_1.mail())         #calling method to return mail id
@@ -49,12 +63,41 @@ print(stu_3.mail())         #checking mail id of object created using alternativ
 
 print(stu_1.__dict__) #to get all the attributes of the object
 
+print(Students.is_adult(20))  #calling static method using class name
+ 
+print(stu_4.student_no)  #calling static method using subclass name
+
+
 
 
 
 #File objects
 #----------------------------------------------
-f = open('test.txt', 'r')  #opening file in read mode
+# f = open('test.txt', 'r')  #opening file in read mode
        #OR
   #CONTEXT MANAGER
-with open('test.txt', 'r') as f:  #using context manager to open     
+# with open('test.txt', 'w') as f:  #using context manager to open(it closes the file itself automatically)
+#       f.write("Hello World")         #writing to the file
+
+with open('test.txt', 'r') as f:  #using context manager to open(it closes the file itself automatically)
+    content = f.read()           #reading the content of file
+    print(content)               #printing the content of file
+
+    #readline() - reads single line from the file
+    #readlines() - reads all lines and returns list of lines
+    #write() - writes string to the file
+    #end = '' in print to avoid new line after printing
+
+    #various function like
+    # tell() - returns current position of file cursor
+    # seek() - changes the position of file cursor to given index
+    print(f"Length of content: {len(content)}")
+    print(f"Current cursor position: {f.tell()}")
+    f.seek(0)  #moving cursor to starting
+    print(f"Cursor position after seek(0): {f.tell()}")
+
+
+    with open('test.txt', 'r') as rf:
+        with open('copy.txt', 'w') as wf:
+            for line in rf:
+                wf.write(line)   #copying content from one file to another  
